e4cf9722fde8fb92bdcd8edbd710fab085179b67
jplimeira <jayropl777@gmail.com>
2026-04-15 14:42:39 -0300

    ajuste de comentário

--- JS/aula 10 functions/functions.js
+++ JS/aula 10 functions/functions.js
@@ -5,10 +5,12 @@
 
 const variavel = "abacatinho"
 
+// Function:
 function nomeNaTela() {
     console.log(variavel)
 }
 
 // ao digitar o nome da funcao, ela é executada
-// e se não chamar, o js a ignora e executa apenas se solicitada 
+// e se não chamar, o js a ignora e executa apenas se solicitada
+
 nomeNaTela()
