cb0303b5d7ffbc1b8c23cd8db772cd2efe792a44
jplimeira <jayropl777@gmail.com>
2026-04-20 16:25:57 -0300

    ajustes

--- JS/aula 13 querySelector/selector.js
+++ JS/aula 13 querySelector/selector.js
@@ -11,3 +11,10 @@ console.log(elements)
 // para trazer um ID, é necessário usar o # antes do nome da mesma
 const elements = document.querySelector("#main-input")
 console.log(elements)
+
+/*
+ para pegar exatamente o item desejado de uma classe, basta especificar o tipo de item e após inserir o classe
+para trazer um ID, é necessário usar o # antes do nome da mesma (EXEMPLO)
+*/
+const elements = document.querySelectorAll("buttonm.main-input")
+console.log(elements)
