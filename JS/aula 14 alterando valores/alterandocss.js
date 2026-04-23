396e9ddb14b93eacb57a6955bfb1a853132bd2c0
jplimeira <jayropl777@gmail.com>
2026-04-22 22:01:01 -0300

    Adição de mais anotações referente a alteração de dados em HTML e CSS através do JavaScript.

---
+++ JS/aula 14 alterando valores/alterandocss.js
@@ -0,0 +1,14 @@
+// Apenas alguns exemplos de possibilidades de mudanças de CSS através do código JS
+
+const button = document.querySelector(".main-button")
+
+button.style.backgroundColor = "grey"
+
+const h1 = document.querySelector("h1")
+
+h1.style.color = "Red"
+
+const img = document.querySelector("img")
+
+img.style.width = "500px"
+
---
+++ JS/aula 14 alterando valores/alterandotextosehtml.js
@@ -0,0 +1,5 @@
+const element10 = document.querySelector(".paragraph-js")
+
+console.log(element10.textContent) // TRAZ APENAS O QUE ESTÁ EM HTML
+console.log(element10.innerText) // MOSTRA APENAS O QUE CONSTA EM CSS 
+console.log(element10.innerHTML) // TRÁS TUDO E PERMITE ADICIONAR HTML AO ALTERAR USANDO ELE
